90940fb9e824beb76cbc5b2f35b5f5b4ce5f1ffe
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-06 11:01:57 -0400

    added tailwindcss

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import React from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const MyApp = ({Component, pageProps}) => {
+  return (
+    <>
+    <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp

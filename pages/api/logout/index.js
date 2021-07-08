7944061915fb47d1393c27d92e4e5b8922141ddc
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-08 14:49:12 -0400

    added logout api route

---
+++ pages/api/logout/index.js
@@ -0,0 +1,23 @@
+import cookie from 'cookie';
+
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        res.setHeader(
+            'Set-Cookie',
+            cookie.serialize('token', '', {
+                httpOnly: true,
+                secure: process.env.NODE_ENV !== 'development',
+                expires: new Date(0),
+                sameSite: 'strict',
+                path: '/',
+            })
+        );
+        res.status(200).send({
+            message: 'Signed Out.',
+        });
+    } else {
+        res.status(500).send({
+            message: 'This is not a POST request.',
+        });
+    }
+}

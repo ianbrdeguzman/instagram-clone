34a035299870a3f635dc3bf757d260f6d769357c
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-11 10:20:03 -0400

    added token and refresh token upon login

--- pages/api/login/index.js
+++ pages/api/login/index.js
@@ -1,7 +1,10 @@
 import dbConnect from '../../../utils/dbConnect';
 import User from '../../../models/userModel';
 import bcrypt from 'bcryptjs';
-import generateToken from '../../../utils/generateToken';
+import {
+    generateRefreshToken,
+    generateToken,
+} from '../../../utils/generateToken';
 import cookie from 'cookie';
 
 export default async function handler(req, res) {
@@ -11,44 +14,41 @@ export default async function handler(req, res) {
 
             const user = await User.findOne({ email: req.body.email });
 
-            if (user) {
-                if (await bcrypt.compare(req.body.password, user.password)) {
-                    const token = generateToken(user);
+            if (!user)
+                throw new Error(
+                    'Sorry, your password was incorrect. Please double-check your password.'
+                );
 
-                    res.setHeader(
-                        'Set-Cookie',
-                        cookie.serialize('token', token, {
-                            httpOnly: true,
-                            secure: process.env.NODE_ENV !== 'development',
-                            maxAge: 60 * 60,
-                            sameSite: 'strict',
-                            path: '/',
-                        })
-                    );
-                    res.status(200).send({
-                        _id: user._id,
-                        email: user.email,
-                        name: user.name,
-                        username: user.username,
-                    });
-                } else {
-                    res.status(404).send({
-                        message:
-                            'Sorry, your password was incorrect. Please double-check your password.',
-                    });
-                }
-            } else {
-                res.status(404).send({
-                    message:
-                        'Sorry, your password was incorrect. Please double-check your password.',
-                });
-            }
+            if (!(await bcrypt.compare(req.body.password, user.password)))
+                throw new Error(
+                    'Sorry, your password was incorrect. Please double-check your password.'
+                );
+
+            const token = generateToken(user);
+            const refreshToken = generateRefreshToken(user);
+
+            res.setHeader(
+                'Set-Cookie',
+                cookie.serialize('refreshToken', refreshToken, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV !== 'development',
+                    maxAge: 60 * 60 * 24,
+                    sameSite: 'strict',
+                    path: '/',
+                })
+            );
+            res.status(200).send({
+                _id: user._id,
+                email: user.email,
+                name: user.name,
+                username: user.username,
+                token: token,
+            });
         } catch (error) {
-            console.log(error);
-            res.status(500).send(error);
+            res.status(404).send(error.message);
         }
     } else {
-        res.status(500).send({
+        res.status(400).send({
             message: 'This is not a POST request.',
         });
     }

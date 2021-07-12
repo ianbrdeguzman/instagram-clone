04caf3db3ba0e77bc8149b544ad1eca4003886d2
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-12 17:37:54 -0400

    added password reset provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import { AuthProvider } from '../context/authContext';
 import { RegisterProvider } from '../context/registerContext';
+import { PasswordResetProvider } from '../context/passwordResetContext';
 
 const MyApp = ({ Component, pageProps }) => {
     return (
         <AuthProvider>
             <RegisterProvider>
-                <Component {...pageProps} />
+                <PasswordResetProvider>
+                    <Component {...pageProps} />
+                </PasswordResetProvider>
             </RegisterProvider>
         </AuthProvider>
     );

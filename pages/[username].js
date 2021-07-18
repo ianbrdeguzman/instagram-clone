6fa06ab9ce8f7090fbb2af41dfc0ad11061ba588
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-18 16:25:08 -0400

    styled profile page

--- pages/[username].js
+++ pages/[username].js
@@ -1,6 +1,9 @@
 import dbConnect from '../utils/dbConnect';
 import User from '../models/userModel';
 import Layout from '../components/Layout';
+import Image from 'next/image';
+import Head from 'next/head';
+import { FiSettings } from 'react-icons/fi';
 
 export const getStaticPaths = async () => {
     try {
@@ -45,11 +48,78 @@ export const getStaticProps = async (context) => {
 
 const Profile = ({ data }) => {
     const user = JSON.parse(data);
-
     return (
         <Layout>
-            <div className='min-h-screen flex justify-center items-center bg-gray-100'>
-                Profile page {user.name}
+            <Head>
+                <title>
+                    {user.name} (@{user.username})
+                </title>
+                <meta
+                    name='viewport'
+                    content='initial-scale=1.0, width=device-width'
+                />
+            </Head>
+            <div className='min-h-screen bg-gray-100'>
+                <header className='pt-[54px] max-w-[975px] mx-auto sm:border-b'>
+                    <div className='p-4 flex items-center sm:py-8'>
+                        {user && user.image && (
+                            <div>
+                                <Image
+                                    src='https://www.w3schools.com/howto/img_avatar.png'
+                                    alt='Avatar'
+                                    layout='fill'
+                                    objectFit='cover'
+                                    quality={100}
+                                />
+                            </div>
+                        )}
+                        {user && !user.image && (
+                            <div className='sm:min-w-[300px] flex justify-center'>
+                                <div className='min-w-[77px] min-h-[77px] sm:min-w-[150px] sm:min-h-[150px] border flex justify-center items-center rounded-full'>
+                                    <h2 className='uppercase text-3xl'>
+                                        {user.name.substring(0, 2)}
+                                    </h2>
+                                </div>
+                            </div>
+                        )}
+                        <section className='w-full pl-4 relative'>
+                            <div className='flex items-center mb-2'>
+                                <h2 className='text-3xl font-light mr-4'>
+                                    {user.username}
+                                </h2>
+                                <button className='hidden sm:block border w-full sm:w-auto px-2 py-1 text-sm rounded font-semibold mr-4'>
+                                    Edit Profile
+                                </button>
+                                <button className='text-xl'>
+                                    <FiSettings />
+                                </button>
+                            </div>
+                            <button className='border w-full sm:w-auto px-2 py-1 text-sm rounded font-semibold sm:hidden'>
+                                Edit Profile
+                            </button>
+                            <ul className='hidden sm:flex'>
+                                <li className='mr-10'>
+                                    <span className='font-semibold'>0</span>{' '}
+                                    posts
+                                </li>
+                                <li className='mr-10'>
+                                    <span className='font-semibold'>54</span>{' '}
+                                    followers
+                                </li>
+                                <li>
+                                    <span className='font-semibold'>27</span>{' '}
+                                    following
+                                </li>
+                            </ul>
+                            <p className='py-2 font-semibold hidden sm:block'>
+                                {user.name}
+                            </p>
+                        </section>
+                    </div>
+                </header>
+                <div className='sm:hidden border-b px-4 pb-4 font-semibold'>
+                    <p>{user.name}</p>
+                </div>
             </div>
         </Layout>
     );

7567649dc8ad077965b176415978612254907feb
ianbrdeguzman <ianbrdeguzman@gmail.com>
2021-07-18 16:26:18 -0400

    fixed drop down menu style

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,106 +24,110 @@ const Header = () => {
     };
 
     return (
-        <header className='border-b w-screen h-[54px] flex justify-between items-center fixed top-0 left-0 bg-white px-[20px]'>
-            <div className='max-w-[975px] flex items-center'>
-                <Link href='/'>
-                    <a className='flex items-center'>
-                        <Image
-                            src='/images/logo.png'
-                            alt='Logo'
-                            width={103}
-                            height={29}
-                        ></Image>
-                    </a>
-                </Link>
-            </div>
-            <div>
-                <form className='relative'>
-                    <span className='hidden sm:block absolute top-1/2 -translate-y-1/2 left-3 text-gray-500'>
-                        <BiSearch />
-                    </span>
-                    <input
-                        type='text'
-                        id='search'
-                        placeholder='Search'
-                        aria-required='true'
-                        autoCapitalize='off'
-                        autoCorrect='off'
-                        className='h-[28px] hidden sm:block text-sm border bg-gray-100 rounded p-2 pl-8'
-                    />
-                </form>
-            </div>
-            <nav className='flex text-2xl'>
-                <Link href='/'>
-                    <a>
-                        <AiOutlineHome />
-                    </a>
-                </Link>
-                <Link href='/'>
-                    <a className='mx-2'>
-                        <FiSend />
-                    </a>
-                </Link>
-                <Link href='/'>
-                    <a>
-                        <AiOutlineCompass />
-                    </a>
-                </Link>
-                <Link href='/'>
-                    <a className='mx-2'>
-                        <AiOutlineHeart />
-                    </a>
-                </Link>
-                <button
-                    onClick={() => setShow(!show)}
-                    className='flex justify-center items-center text-sm m border rounded-full bg-gray-100 w-[28px] h-[28px]'
-                >
-                    <span className='uppercase'>
-                        {user?.username.substring(0, 2)}
-                    </span>
-                </button>
-                {show && (
-                    <ul className='w-[230px] bg-white absolute top-[50px] right-0 text-sm rounded shadow'>
-                        <Link
+        <header className='border-b w-screen fixed top-0 left-0 bg-white px-[20px]'>
+            <div className='flex justify-between items-center h-[54px] max-w-[975px] mx-auto'>
+                <div className='max-w-[975px] flex items-center'>
+                    <Link href='/'>
+                        <a className='flex items-center'>
+                            <Image
+                                src='/images/logo.png'
+                                alt='Logo'
+                                width={103}
+                                height={29}
+                            ></Image>
+                        </a>
+                    </Link>
+                </div>
+                <div>
+                    <form className='relative'>
+                        <span className='hidden sm:block absolute top-1/2 -translate-y-1/2 left-3 text-gray-500'>
+                            <BiSearch />
+                        </span>
+                        <input
+                            type='text'
+                            id='search'
+                            placeholder='Search'
+                            aria-required='true'
+                            autoCapitalize='off'
+                            autoCorrect='off'
+                            className='h-[28px] hidden sm:block text-sm border bg-gray-100 rounded p-2 pl-8'
+                        />
+                    </form>
+                </div>
+                <nav className='flex text-2xl'>
+                    <Link href='/'>
+                        <a>
+                            <AiOutlineHome />
+                        </a>
+                    </Link>
+                    <Link href='/'>
+                        <a className='mx-2'>
+                            <FiSend />
+                        </a>
+                    </Link>
+                    <Link href='/'>
+                        <a>
+                            <AiOutlineCompass />
+                        </a>
+                    </Link>
+                    <Link href='/'>
+                        <a className='mx-2'>
+                            <AiOutlineHeart />
+                        </a>
+                    </Link>
+                    <div className='relative'>
+                        <button
                             onClick={() => setShow(!show)}
-                            href={`/${user?.username}`}
+                            className='flex justify-center items-center text-sm m border rounded-full bg-gray-100 w-[28px] h-[28px]'
                         >
-                            <a>
+                            <span className='uppercase'>
+                                {user?.username.substring(0, 2)}
+                            </span>
+                        </button>
+                        {show && (
+                            <ul className='w-[230px] bg-white absolute top-[40px] right-0 text-sm rounded shadow'>
+                                <Link
+                                    onClick={() => setShow(!show)}
+                                    href={`/${user?.username}`}
+                                >
+                                    <a>
+                                        <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
+                                            <span className='mr-2 text-base'>
+                                                <CgProfile />
+                                            </span>
+                                            Profile
+                                        </li>
+                                    </a>
+                                </Link>
                                 <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
                                     <span className='mr-2 text-base'>
-                                        <CgProfile />
+                                        <BiBookmark />
                                     </span>
-                                    Profile
+                                    Saved
                                 </li>
-                            </a>
-                        </Link>
-                        <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
-                            <span className='mr-2 text-base'>
-                                <BiBookmark />
-                            </span>
-                            Saved
-                        </li>
-                        <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
-                            <span className='mr-2 text-base'>
-                                <FiSettings />
-                            </span>
-                            Settings
-                        </li>
-                        <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
-                            <span className='mr-2 text-base'>
-                                <HiSwitchHorizontal />
-                            </span>
-                            Switch Accounts
-                        </li>
-                        <li
-                            onClick={onLogOut}
-                            className='py-2 px-4 border-t cursor-pointer hover:bg-gray-100'
-                        >
-                            Log Out
-                        </li>
-                    </ul>
-                )}
-            </nav>
+                                <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
+                                    <span className='mr-2 text-base'>
+                                        <FiSettings />
+                                    </span>
+                                    Settings
+                                </li>
+                                <li className='py-2 px-4 cursor-pointer hover:bg-gray-100 flex items-center'>
+                                    <span className='mr-2 text-base'>
+                                        <HiSwitchHorizontal />
+                                    </span>
+                                    Switch Accounts
+                                </li>
+                                <li
+                                    onClick={onLogOut}
+                                    className='py-2 px-4 border-t cursor-pointer hover:bg-gray-100'
+                                >
+                                    Log Out
+                                </li>
+                            </ul>
+                        )}
+                    </div>
+                </nav>
+            </div>
         </header>
     );
 };
